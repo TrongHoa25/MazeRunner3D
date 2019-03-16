857994051e37e14584dee625689596bbeaee935d
TrongHoa25 <nhokteti2509@gmail.com>
2019-03-16 11:54:09 +0700

    First-Settup

---
+++ Assets/scritpts/playermovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody player;
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        player.AddForce(movement * speed);
+    }
+
+}
