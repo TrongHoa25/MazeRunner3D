04f79b931753d389d8378b4f394db1a8fa49fad8
TrongHoa25 <nhokteti2509@gmail.com>
2019-03-19 08:35:21 +0700

    First develop

---
+++ Assets/scritpts/bedamage.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class bedamage : MonoBehaviour
+{
+    private Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            anim.SetTrigger("BeDamage");
+        }
+    }
+}
