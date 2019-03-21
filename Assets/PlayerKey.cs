7193479ec5c257a6e14ae73e2ca15b4c69b5659a
TrongHoa25 <nhokteti2509@gmail.com>
2019-03-21 23:47:28 +0700

    soundv1

--- Assets/PlayerKey.cs
+++ Assets/PlayerKey.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 public class PlayerKey : MonoBehaviour
 {
     public int key = 0;
-    public Text counttext;
+  
     // Start is called before the first frame update
    public void setKey()
     {
         if(key <= 3){
             key++;
-            counttext.text = "Key: " + this.key.ToString();
         }
+    }
+
+    public int getKeysNum()
+    {
+        return this.key;
     }
 }
